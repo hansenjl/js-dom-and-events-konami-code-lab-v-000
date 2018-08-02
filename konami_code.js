66c32ddc4f38fd49e8016f29c2a60e76dbc4349a
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-08-02 20:22:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const input = document.querySelector('input')
+ 
+  document.body.addEventListener('keydown', function(e) {
+    if (e.which === 71) {
+      return e.preventDefault()
+    }
+  })
 
 }
