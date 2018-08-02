dd06a6c608bd4f4c0c3f5182fa5cb3501289747f
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-08-02 20:23:52 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   document.body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which)
-    if (e.which === 71) {
+    if (key === code[index]) {
       return e.preventDefault()
     }
   })
